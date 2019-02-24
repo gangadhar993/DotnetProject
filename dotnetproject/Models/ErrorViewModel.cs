aa657015f5ccabf89efaf0babebf8ccd54551e3a
Gangadhar Adusumalli <gangadharadusumalli1993@gmail.com>
2019-02-24 08:52:27 -0600

    initial commit

---
+++ dotnetproject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnetproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
