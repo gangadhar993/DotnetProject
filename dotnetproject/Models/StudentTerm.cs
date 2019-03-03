cd68e2b45263d5ad70f074c304757b8e39043e5b
s533491 <s533491@NWMISSOURI.EDU>
2019-03-03 17:01:37 -0600

    Commit Model

---
+++ dotnetproject/Models/StudentTerm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetproject.Models
+{
+    public class StudentTerm
+    {
+        public int StudentTermID { get; internal set; }
+        public int DegreePlanId { get; internal set; }
+        public int Term { get; internal set; }
+        public string TermAbbrev { get; internal set; }
+        public string TermName { get; internal set; }
+    }
+}
